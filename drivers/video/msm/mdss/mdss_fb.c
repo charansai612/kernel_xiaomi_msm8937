f79de6e3448ff09ec4eb883422164eb552173c6d
flar2 <asegaert@gmail.com>
2016-06-26 23:06:11 -0400

    mdss_fb: add backlight dimmer option
    Signed-off-by: charansai612 <charansai612@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -76,6 +76,12 @@
 #define BLANK_FLAG_ULP	FB_BLANK_NORMAL
 #endif
 
+#define MDSS_BRIGHT_TO_BL_DIM(out, v) do {\
+			out = (12*v*v+1393*v+3060)/4465;\
+			} while (0)
+bool backlight_dimmer = false;
+module_param(backlight_dimmer, bool, 0755);
+
 static struct fb_info *fbi_list[MAX_FBI_LIST];
 static int fbi_list_index;
 
@@ -283,20 +289,15 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 	if (value > mfd->panel_info->brightness_max)
 		value = mfd->panel_info->brightness_max;
 
-	/* This maps android backlight level 0 to 255 into
-	   driver backlight level 0 to bl_max with rounding */
-	#if 1
-	if (mfd->panel_info->bl_min == 1)
-		mfd->panel_info->bl_min = 5;
-	MDSS_BRIGHT_TO_BL1(bl_lvl, value, mfd->panel_info->bl_min, mfd->panel_info->bl_max,
-			brightness_min, mfd->panel_info->brightness_max);
-	if (bl_lvl && !value)
-		bl_lvl = 0;
+	if (backlight_dimmer) {
+		MDSS_BRIGHT_TO_BL_DIM(bl_lvl, value);
+	} else {
+		/* This maps android backlight level 0 to 255 into
+		   driver backlight level 0 to bl_max with rounding */
+		MDSS_BRIGHT_TO_BL(bl_lvl, value, mfd->panel_info->bl_max,
+					mfd->panel_info->brightness_max);
+	}
 
-	#else
-	MDSS_BRIGHT_TO_BL(bl_lvl, value, mfd->panel_info->bl_max,
-				mfd->panel_info->brightness_max);
-        #endif
 	if (!bl_lvl && value)
 		bl_lvl = 1;
 	pr_debug("bl_lvl is %d, value is %d\n", bl_lvl, value);

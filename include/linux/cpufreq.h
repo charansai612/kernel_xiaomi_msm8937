ce2f3cdfc3f52e1d0d30206cd8659828026b1fca
Zane Zaminsky <cyxman@yahoo.com>
2017-03-15 23:16:25 -0700

    Add ZZMoove Governor
    Signed-off-by: charansai612 <charansai612@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -510,9 +510,15 @@ extern struct cpufreq_governor cpufreq_gov_lionfish;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+<<<<<<< HEAD
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
+=======
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
+>>>>>>> 558f07f... Add ZZMoove Governor
 #endif
 
 /*********************************************************************
